a5628bdac17005fcfd3b28baa3a9e7c20109fd16
Tran Khoa <khoatranpc603@gmail.com>
2023-07-14 11:48:58 +0700

    update env

--- src/database/index.ts
+++ src/database/index.ts
@@ -17,7 +17,7 @@ const getUri = () => {
     const AUTH_DB = MONGO_USERNAME && MONGO_PASSWORD ? `${MONGO_USERNAME}:${MONGO_PASSWORD}@` : '';
     // const uri = `${MONGO_CLIENT}://${AUTH_DB}${MONGO_HOST}:${MONGO_PORT}/${DB_NAME}`;
     // const uri = `mongodb+srv://cuongnv:UCENc4FEl5ht0jW4@mindxdev.dmpv4mn.mongodb.net/mindx-k18`
-    const uri = `mongodb://127.0.0.1:27017/mindx-k18-build`;
+    const uri = process.env.DB_CLOUD as string;
     return uri;
 }
 export default getUri;
