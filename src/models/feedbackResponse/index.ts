81e9945a97fef9d146abd20decc7bfdddbf7c1cb
Tran Khoa <khoatranpc603@gmail.com>
2023-08-02 16:16:03 +0700

    add fields

--- src/models/feedbackResponse/index.ts
+++ src/models/feedbackResponse/index.ts
@@ -24,6 +24,30 @@ const feedbackResponseSchema = new mongoose.Schema({
         type: mongoose.SchemaTypes.ObjectId,
         ref: Collections.BOOKTEACHER,
         required: true
+    },
+    pointCxo: {
+        type: Number,
+        required: true
+    },
+    pointST: {
+        type: Number,
+        required: true
+    },
+    pointMT: {
+        type: Number,
+        required: true
+    },
+    pointOb: {
+        type: Number,
+        required: true
+    },
+    pointSyl: {
+        type: Number,
+        required: true
+    },
+    docDetail: {
+        type: String,
+        required: true
     }
 }, {
     timestamps: true
