102b0dd0caa18584462303f6039c4df81c15f93f
Tran Khoa <khoatranpc603@gmail.com>
2023-11-25 01:00:35 +0700

    refactor

--- src/routes/feedback.ts
+++ src/routes/feedback.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import feedbackController from '../controllers/feedback';
 import feedbackResponseController from '../controllers/feedbackResponse';
 import middlewares from '../middlewares';
+import { ROLE } from '../global/enum';
 
 const FeedbackRouter = Router();
 FeedbackRouter.get('/list-class', middlewares.verifyJWT, middlewares.isTE, feedbackController.getRecordByMonth);
@@ -9,6 +10,6 @@ FeedbackRouter.put('/:feedbackId', middlewares.verifyJWT, middlewares.isTE, feed
 FeedbackRouter.get('/form/class', feedbackController.getClassForStudentFormFeedback);
 FeedbackRouter.get('/form/class/:classId/group', feedbackController.getGroupInClass);
 FeedbackRouter.post('/response', feedbackResponseController.sendResponseFromForm);
-FeedbackRouter.get('/response', middlewares.verifyJWT, middlewares.isTE, feedbackResponseController.getListRecordResponse);
+FeedbackRouter.get('/response', middlewares.verifyJWT, middlewares.acceptRole(undefined, ROLE.TE, ROLE.TEACHER), feedbackResponseController.getListRecordResponse);
 FeedbackRouter.get('/teacher/response', middlewares.verifyJWT, middlewares.isTE, feedbackResponseController.getListRecordResponseByTeacherId);
 export default FeedbackRouter;
--- src/routes/te.ts
+++ src/routes/te.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import middlewares from '../middlewares';
 import teController from '../controllers/te';
+import { ROLE } from '../global/enum';
 
 const TERouter = Router();
-TERouter.get('', middlewares.verifyJWT, middlewares.isTE, teController.getBySingleField);
+TERouter.get('', middlewares.verifyJWT, middlewares.acceptRole(undefined, ROLE.TE, ROLE.TEACHER), teController.getBySingleField);
 TERouter.get('/:accountId', middlewares.verifyJWT, middlewares.isTE, teController.getByAccountId);
 TERouter.post('', middlewares.verifyJWT, middlewares.isTE, teController.createTeInfo);
 
