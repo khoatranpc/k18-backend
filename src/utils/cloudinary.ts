074c3345a84482893ab35a5e823c01bc673fea7b
Tran Khoa <khoatranpc603@gmail.com>
2023-12-26 20:27:45 +0700

    hot fix

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -1,4 +1,5 @@
 import { v2 as cloudinary } from 'cloudinary';
+import crypto from 'crypto';
 cloudinary.config({
     cloud_name: 'dxo374ch8',
     api_key: '399236672491454',
@@ -8,7 +9,7 @@ const uploadToCloud = async (file: Express.Multer.File) => {
     const dataUrl = `data:${file.mimetype};base64,${file.buffer.toString('base64')}`;
     const fileName = file.originalname.split('.')[0];
     const uploaded = await cloudinary.uploader.upload(dataUrl, {
-        public_id: fileName,
+        public_id: fileName + crypto.randomUUID(),
         resource_type: 'auto',
     })
     return uploaded;
